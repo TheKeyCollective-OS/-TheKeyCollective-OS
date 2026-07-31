66295ab38c8aee4f94f6af28760395de112af181
TheKeyCollective-OS <mitchelllkey@gmail.com>
2026-07-30 18:02:51 -0700

    Fix Lani photo slideshow controls

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ import {enhanceSprint6B47} from './sprint6b47.js?v=16.48.2';
 import {enhanceSprint6B48} from './sprint6b48.js?v=16.48.2';
 import {enhanceSprint6B49} from './sprint6b49.js?v=16.49.0';
 import {enhanceSprint6C1} from './sprint6c1.js?v=16.52.4';
-import {enhanceSprint6C3} from './sprint6c3.js?v=16.53.2';
+import {enhanceSprint6C3} from './sprint6c3.js?v=16.53.3';
 
 patchPages(pages);
 patchPagesSprint5(pages);
