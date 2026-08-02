2337b818080f17e5096f44808338c2eae3faee2d
TheKeyCollective-OS <mitchelllkey@gmail.com>
2026-08-01 21:54:19 -0700

    Refresh cached Lani isolation fix

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ import {patchPagesSprint6B24,enhanceSprint6B24} from './sprint6b24.js';
 import {patchPagesSprint6B25,enhanceSprint6B25} from './sprint6b25.js';
 import {patchPagesSprint6B26,enhanceSprint6B26} from './sprint6b26.js?v=16.43.7';
 import {enhanceSprint6B43} from './sprint6b43.js?v=16.44.7';
-import {enhanceSprint6B44} from './sprint6b44.js?v=16.44.9';
+import {enhanceSprint6B44} from './sprint6b44.js?v=16.58.43';
 import {enhanceSprint6B45} from './sprint6b45.js?v=16.45.1';
 import {enhanceSprint6B46} from './sprint6b46.js?v=16.46.1';
 import {enhanceSprint6B47} from './sprint6b47.js?v=16.48.2';
@@ -41,7 +41,7 @@ import {enhanceSprint6B48} from './sprint6b48.js?v=16.48.2';
 import {enhanceSprint6B49} from './sprint6b49.js?v=16.55.53';
 import {enhanceSprint6C1} from './sprint6c1.js?v=16.58.37';
 import {enhanceSprint6C3} from './sprint6c3.js?v=16.54.0';
-import {enhanceSprint6D} from './sprint6d.js?v=16.58.37';
+import {enhanceSprint6D} from './sprint6d.js?v=16.58.43';
 
 patchPages(pages);
 patchPagesSprint5(pages);
